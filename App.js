f5ea3eeea15ab7363891b4a03991e5a611fd9822
Soumya323 <soumyaripan@gmail.com>
2023-09-08 20:31:13 +0530

    added parcel while learning how cool and important it is to improve app performance and optimization

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
-const heading1 = React.createElement("h1", {id:"heading1", key:"1"}, "I am h1 tag.");
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+
+const heading1 = React.createElement("h1", {id:"heading1", key:"1"}, "I am h1 tag. do you know me 🚀");
 const heading2 = React.createElement("h1", {id:"heading2", key:"2"}, "No I am the h1 tag.");
 const child = React.createElement("div", {id:"child"},[heading1, heading2] );
 const parent = React.createElement("div", {id:"parent"}, child );
